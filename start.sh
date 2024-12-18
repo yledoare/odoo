7c43247c11cfbc27ec623a3cfc7e88518c4a30d4
Yann Le Doaré <yann@linuxconsole.org>
2024-12-18 07:36:20 +0100

    Install lessc

--- start.sh
+++ start.sh
@@ -10,17 +10,18 @@ fi
 
 if [ ! -e usr ]
 then
-	for PKG in python-reportlab python3-psycopg2 python3-psutil libffi6 python3-pip python3-distutils python3-venv python-pip-whl python3.7 libpython3.7-stdlib python3.7-minimal python3.7-venv libpython3.7-minimal # python3-minimal libpython3-stdlib python3-venv
+	for PKG in libicu63 libnode64 nodejs node-less python3-pyldap python3-vatnumber python3-suds python3-gevent python3-feedparser python3-html2text python3-reportlab python3-psycopg2 python3-psutil libffi6 python3-pip python3-distutils python3-venv python-pip-whl python3.7 libpython3.7-stdlib python3.7-minimal python3.7-venv libpython3.7-minimal
 	do
 		[ -e download ] && rm download
-  		wget https://packages.debian.org/buster/amd64/$PKG/download 
+  		wget -q https://packages.debian.org/buster/amd64/$PKG/download 
 
 		URL=$(grep ftp.fr.debian.org download | cut -d '"' -f2)
 		if [ "$URL" = "" ]
 		then
   			URL=$( grep http://security.debian.org/debian-security/ download  | cut -d '"' -f2)
 		fi
-		wget $URL
+		echo "\n \n Get $URL .. "
+		wget -q $URL
 		pwd
 		ls
 		DEB=$(basename $URL)
@@ -28,6 +29,8 @@ then
 		rm $DEB
 	done
 	find usr/lib/python3.7 -type f -print0 | xargs -0 sed -i "s|usr|$PWD/usr|g"
+        sed -i "s|usr|$PWD/usr|g" usr/bin/lessc 
+        sed -i "s|../lib/less|$PWD/usr/lib/nodejs/less/lib/less|g" usr/bin/lessc 
 fi
 
 export PATH=$PWD/usr/bin:$PWD/usr/local/bin:$PATH
@@ -39,13 +42,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/usr/lib/x86_64-linux-gnu/
 python3.7 -m pip install setuptools
 python3.7 -m pip install wheel
 python3.7 -m pip install markupsafe==2.0.1
+
 # [ ! -e venv ] && python3.7 -m venv venv
 #[ $? = 1 ] && exit 1
 # . venv/bin/activate
 [ ! -e odoo-$ODOO ] && git clone --depth 1 -b 11.0 https://github.com/odoo/odoo && mv odoo odoo-$ODOO
 #[ -e /home/linuxconsole2024/x86_64/ ] && grep psycopg2 odoo-$ODOO/requirements.txt && cd odoo-$ODOO && pwd && patch -p1 < ../linuxconsole-odoo.patch && cd ..
-python3.7 -m pip install -r odoo-11/requirements.txt
+python3.7 -m pip install -r odoo-11/requirements.txt || exit 1
 
 install -d addons
+
 #[ -e /home/linuxconsole2024/x86_64/lib/python3.10/site-packages/ ] && export PYTHONPATH=/home/linuxconsole2024/x86_64/lib/python3.10/site-packages/:$PYTHONPATH
 python3.7 ./odoo-$ODOO/odoo-bin -d odoo-$ODOO --db_host localhost -r odoo -w odoo -i base # --addons-path=$PWD/addons,$PWD/odoo/addons #-i
