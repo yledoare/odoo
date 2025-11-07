5f6c771e1b459f465f2811d864d36932f63fec46
Yann Le Doaré <yann@linuxconsole.org>
2025-11-07 09:07:55 +0100

    skip comments

--- include/addons.sh
+++ include/addons.sh
@@ -4,12 +4,15 @@ then
     cd addons-$ODOO
 	ls ../addons | while read ADDON
 	do
+    [ -e $ADDON ] && continue
 	  install -d $ADDON
 	  cd $ADDON
           echo "OPT=\$OPT,$PWD/$ADDON" >> ../../opt.txt
 	  cat ../../addons/$ADDON | while read GIT
 	  do
-		git clone --depth 1 -b $ODOO".0" $GIT
+      FIRST=$(echo $GIT | cut -d' ' -f1) 
+      # Skip commented line
+		  [ "$FIRST" != "#" ] && git clone --depth 1 -b $ODOO".0" $GIT
 	  done
 	  cd ..
 	done
