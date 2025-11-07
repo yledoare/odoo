3fa571f3c7e771c42d940a5d79c385c581f54b64
Yann Le Doaré <yann@linuxconsole.org>
2025-11-07 08:45:36 +0100

    include/wkhtmltopdf.sh

--- start.sh
+++ start.sh
@@ -5,25 +5,9 @@ ODOO="18"
 which pg_config || sudo apt-get install -y postgresql-common libpq-dev python3-dev libldap2-dev  libsasl2-dev
 which pg_config || exit 2
 
-DEBIAN=""
-[ -e /etc/debian_version ] && grep trixie /etc/debian_version && DEBIAN="TRIXIE"
+. include/postgres.sh
+. include/wkhtmltopdf.sh
 
-if [ "$DEBIAN" = "TRIXIE" ] || [ -e /home/linuxconsole2024/x86_64/lib ]
-then
-	if [ ! -e usr ]
-	then
-	  for PKG in libjpeg62-turbo
-	  do
-		install-local-deb bookworm $PKG
-	  done
-	  wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
-	  dpkg -x wkhtmltox_0.12.6.1-3.bookworm_amd64.deb .
-	fi
-export PATH=$PWD/usr/bin:$PWD/usr/local/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/usr/lib/x86_64-linux-gnu/
-fi
-
-wkhtmltopdf -V || exit 1
 
 if [ $ODOO == "12" ] || [ $ODOO == "13" ] || [ $ODOO == "14" ]
 then
